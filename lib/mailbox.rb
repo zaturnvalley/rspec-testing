eedab4f8df1629ade0dccf1bacaadca900e50be1
zaturnvalley <zacharycmason@gmail.com>
2016-08-30 14:35:08 -0700

    passed all rspec tests in regards to ruby classes

--- lib/mailbox.rb
+++ lib/mailbox.rb
@@ -1,13 +1,21 @@
 class MailBox
+  attr_reader :letters, :packages
+
   def initialize(letters, packages)
+    @letters = letters
+    @packages = packages
   end
 
   def add_letter
+    @letters += 1
   end
 
   def add_package
+    @packages += 1
   end
 
   def clear
+    @packages = 0
+    @letters = 0
   end
 end
--- lib/tree.rb
+++ lib/tree.rb
@@ -1,7 +1,13 @@
 class Tree
+  attr_accessor :type, :height, :age
+
   def initialize(type, height, age)
+    @type = type
+    @height = height
+    @age = age
   end
 
   def grow_tree(water_amount)
+    @height += water_amount / 10
   end
 end
